34bc5843de1a2b5689c4625e633dc9d229522ea0
nelsontky <nelson@ccb.wtf>
2021-10-04 08:33:50 +0000

    feat: setup api gateway

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { CatsModule } from "./cats/cats.module";
+
+@Module({
+  imports: [ConfigModule.forRoot(), CatsModule],
+})
+export class AppModule {}
