0c24a6ad3bf8cb33bf0475345feb8840eaa5b227
nelsontky <nelson@ccb.wtf>
2021-10-11 13:12:44 +0000

    refactor: minor dashboard logic change

--- client/src/common/components/TopRainbowBar.tsx
+++ client/src/common/components/TopRainbowBar.tsx
@@ -3,12 +3,16 @@ import { Box } from "@mui/material";
 export default function TopRainbowBar() {
   return (
     <Box sx={{ display: "flex", width: "100%", height: "8px" }}>
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"purple.main"} />
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"red.main"} />
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"yellow.main"} />
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"green.main"} />
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"blue.main"} />
-      <Box sx={{ display: "flex", flex: "1" }} bgcolor={"violet.main"} />
+      {[
+        "purple.main",
+        "red.main",
+        "yellow.main",
+        "green.main",
+        "blue.main",
+        "violet.main",
+      ].map((color) => (
+        <Box key={color} sx={{ display: "flex", flex: "1" }} bgcolor={color} />
+      ))}
     </Box>
   );
 }
