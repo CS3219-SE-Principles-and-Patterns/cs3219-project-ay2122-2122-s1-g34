3380b2037f42e0c92976c6b3750d857d236a414b
nelsontky <nelson@ccb.wtf>
2021-10-11 12:58:36 +0000

    refactor: change min height to 100%

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Box } from "@mui/material";
 import { Switch, Route } from "react-router-dom";
 
 import TopRainbowBar from "common/components/TopRainbowBar";
@@ -7,12 +7,10 @@ import Login from "features/auth/Login";
 import PrivateRoute from "features/auth/PrivateRoute";
 import Register from "features/auth/Register";
 import { useUser } from "features/auth/use-user.hook";
-
 import Collaboration from "features/collaboration/Collaboration";
 import Dashboard from "features/dashboard/Dashboard";
 import Leaderboard from "features/leaderboard/Leaderboard";
 import PastAttempts from "features/past-attempts/PastAttempts";
-
 import AppSnackbar from "features/snackbar/AppSnackbar";
 
 export default function App() {
@@ -24,39 +22,41 @@ export default function App() {
   }
 
   return (
-    <div
-      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
-    >
+    <>
       <CssBaseline />
-      <AppSnackbar />
-      <Switch>
-        <Route path="/collaboration">
-          <Collaboration />
-        </Route>
-        <Route path="/login">
-          <TopRainbowBar />
-          <Login />
-        </Route>
-        <Route path="/register">
-          <TopRainbowBar />
-          <Register />
-        </Route>
+      <Box
+        sx={{ minHeight: "100%", display: "flex", flexDirection: "column" }}
+      >
+        <AppSnackbar />
+        <Switch>
+          <Route path="/collaboration">
+            <Collaboration />
+          </Route>
+          <Route path="/login">
+            <TopRainbowBar />
+            <Login />
+          </Route>
+          <Route path="/register">
+            <TopRainbowBar />
+            <Register />
+          </Route>
 
-        <PrivateRoute path="/">
-          <TopRainbowBar />
-          <Dashboard />
-        </PrivateRoute>
+          <PrivateRoute path="/">
+            <TopRainbowBar />
+            <Dashboard />
+          </PrivateRoute>
 
-        <PrivateRoute path="/past-attempts">
-          <TopRainbowBar />
-          <PastAttempts />
-        </PrivateRoute>
+          <PrivateRoute path="/past-attempts">
+            <TopRainbowBar />
+            <PastAttempts />
+          </PrivateRoute>
 
-        <PrivateRoute path="/leaderboard">
-          <TopRainbowBar />
-          <Leaderboard />
-        </PrivateRoute>
-      </Switch>
-    </div>
+          <PrivateRoute path="/leaderboard">
+            <TopRainbowBar />
+            <Leaderboard />
+          </PrivateRoute>
+        </Switch>
+      </Box>
+    </>
   );
 }
