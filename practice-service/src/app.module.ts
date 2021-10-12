227855c2912b2764492da3d13f569bbed6066f53
nelsontky <nelson@ccb.wtf>
2021-10-12 03:17:20 +0000

    feat(practice): auto delete sessions older than 15 seconds

--- practice-service/src/app.module.ts
+++ practice-service/src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import ormconfig from "./ormconfig";
 import { QuestionsModule } from "./questions/questions.module";
-import { SessionsModule } from './sessions/sessions.module';
+import { SessionsModule } from "./sessions/sessions.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     TypeOrmModule.forRoot(ormconfig),
     QuestionsModule,
     SessionsModule,
