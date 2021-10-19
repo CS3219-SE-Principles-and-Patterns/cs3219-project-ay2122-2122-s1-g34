95ca8e0c39e743b3fe3b0cfbdf8dd1170a90d8df
nelsontky <nelson@ccb.wtf>
2021-10-19 17:52:50 +0000

    feat: add code running logic

--- code-runner-service/src/code-runner/code-runner.service.ts
+++ code-runner-service/src/code-runner/code-runner.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { VM } from "vm2";
 
 @Injectable()
-export class CodeRunnerService {}
+export class CodeRunnerService {
+  private readonly vm: VM;
+
+  constructor() {
+    const timeout = 1000 * 10; // 10 second timeout
+    this.vm = new VM({ timeout, eval: false, wasm: false, fixAsync: true });
+  }
+
+  runCode(code: string) {
+    return this.vm.run(code);
+  }
+}
