d1e95cf92886261dd4040e3d944c44f8caa75be4
nelsontky <nelson@ccb.wtf>
2021-10-21 14:14:43 +0000

    feat: link chat backend to frontend

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
+import { ChatModule } from "./chat/chat.module";
 import { CodeRunnerModule } from "./code-runner/code-runner.module";
 import { CollaborationModule } from "./collaboration/collaboration.module";
-import { ChatModule } from "./chat/chat.module";
 import { FirebaseModule } from "./firebase/firebase.module";
 import { PracticeModule } from "./practice/practice.module";
 import { UsersModule } from "./users/users.module";
@@ -11,15 +11,12 @@ import { UsersModule } from "./users/users.module";
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ChatModule,
     CodeRunnerModule,
     CollaborationModule,
     FirebaseModule,
     PracticeModule,
     UsersModule,
-    ChatModule
   ],
 })
 export class AppModule {}
-
-
-
--- api-gateway/src/chat/chat.gateway.ts
+++ api-gateway/src/chat/chat.gateway.ts
@@ -1,25 +1,27 @@
 import {
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-    MessageBody,
-    ConnectedSocket
-   } from '@nestjs/websockets';
-   import { Socket, Server } from 'socket.io';
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
 
-   import { ChatService } from './chat.service';
-   
-   @WebSocketGateway()
-   export class ChatGateway {
-   
-    @WebSocketServer() server: Server
-   
-    constructor(
-        private readonly chatService: ChatService
-      ) {}
+import { ChatService } from "./chat.service";
+import { ChatDto } from "./dto/chat.dto";
 
-    @SubscribeMessage('chat:send_message') //for a socket to send msg
-    handleChat(@ConnectedSocket() client: Socket,@MessageBody() payload: Object) {
-        this.chatService.handleChat(client, payload);
-    }
-   }
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly chatService: ChatService) {}
+
+  @SubscribeMessage("chat:message")
+  async onMessage(
+    @MessageBody() chatDto: ChatDto,
+    @ConnectedSocket() client: Socket
+  ) {
+    this.chatService.onMessage(chatDto, client, this.server);
+  }
+}
