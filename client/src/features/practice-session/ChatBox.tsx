38fe3c81d817e978b0b14fa2e7f8069db5749e04
nelsontky <nelson@ccb.wtf>
2021-10-21 14:16:40 +0000

    feat: link chat backend to frontend

--- client/src/features/practice-session/ChatBox.tsx
+++ client/src/features/practice-session/ChatBox.tsx
@@ -1,101 +1,117 @@
-import { Box, BoxProps, Stack, Typography, TextField, useTheme, TextFieldProps} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import {
+  Box,
+  BoxProps,
+  Stack,
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import React, { useRef, useEffect } from "react";
+
 import { useAppSelector } from "common/hooks/use-redux.hook";
-import { selectUser } from "features/auth/user.slice";
-import SendIcon from '@mui/icons-material/Send';
-import React, {useRef, useEffect} from "react";
 import { useSocket } from "common/hooks/use-socket.hook";
 
+import { selectUser } from "features/auth/user.slice";
 
-interface ChatBoxProps extends BoxProps {
-}
+interface ChatBoxProps extends BoxProps {}
 
 export default function ChatBox({ sx, ...rest }: ChatBoxProps) {
-  const { socket } = useSocket();
-
-  const themePalette = useTheme().palette;
+  const [typedMessage, setTypedMessage] = React.useState("");
+  const [messages, setMessages] = React.useState<
+    {
+      username: string;
+      message: string;
+    }[]
+  >([]);
 
   const user = useAppSelector(selectUser);
   const userDisplayName = user ? user.displayName : "";
-
-  const [typedMessage, setTypedMessage] = React.useState("");
-
-  //dummy
-  const [messages, setMessages] = React.useState([{"username":"firstuser","message":"testmessage"}]);
-  // ([{"username":"John Tan","message":"hi, how do you think we should start?"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem"},
-  // {"username":"John Tan","message":"yeskbalbgafa"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem kabfbjksbvksncfkalbfelkgvasgvsga"},
-  // {"username":"John Tan","message":"i think we should start with breaking down the problemsgvsa sbegeagsdb"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problemsefaefjkafbkjfbskflabfaklf"},
-  // {"username":"John Tan","message":"hi, how do you think we should start?"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem"},
-  // {"username":"John Tan","message":"yeskbalbgafa"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem kabfbjksbvksncfkalbfelkgvasgvsga"},
-  // {"username":"John Tan","message":"i think we should start with breaking down the problemsgvsa sbegeagsdb"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problemsefaefjkafbkjfbskflabfaklf"},
-  // {"username":"John Tan","message":"hi, how do you think we should start?"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem"},
-  // {"username":"John Tan","message":"yeskbalbgafa"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem kabfbjksbvksncfkalbfelkgvasgvsga"},
-  // {"username":"John Tan","message":"i think we should start with breaking down the problemsgvsa sbegeagsdb"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problemsefaefjkafbkjfbskflabfaklf"},
-  // {"username":"John Tan","message":"hi, how do you think we should start?"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem"},
-  // {"username":"John Tan","message":"yeskbalbgafa"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problem kabfbjksbvksncfkalbfelkgvasgvsga"},
-  // {"username":"John Tan","message":"i think we should start with breaking down the problemsgvsa sbegeagsdb"},
-  // {"username":"Alice Lee","message":"i think we should start with breaking down the problemsefaefjkafbkjfbskflabfaklf"}]);
-
-  const postMessage = () => {
-    //messages.push({"username":userDisplayName,"message":typedMessage});
-    //setMessages(messages);
-    socket?.emit("chat:send_message",{"username":userDisplayName,"message":typedMessage});
-		setTypedMessage("");
-    scrollToBottom();
-	}
-
-  const Messages = Array.from(messages).map((onemessage) => {
-    if (onemessage.username===userDisplayName){ //user's message
-      return (
-        <><Box>
-          <Typography variant="body1" fontSize="15" fontWeight="500" sx={{ color: `${themePalette.purple.main}` }}>
-            {onemessage.username}
+  const Messages = messages.map((message, i) => {
+    return (
+      <Box key={`${message.message + message.username}-${i}`}>
+        <Box>
+          <Typography
+            variant="body1"
+            fontSize="15"
+            fontWeight="500"
+            sx={{
+              color: `${
+                message.username === userDisplayName
+                  ? "purple.main"
+                  : "yellow.main"
+              }`,
+            }}
+          >
+            {message.username}
           </Typography>
-        </Box><Box sx={{mb:2}}>
-            <Typography variant="body1" fontSize="15" sx={{ color: `${themePalette.black.main}` }}>
-              {onemessage.message}
-            </Typography>
-          </Box></> 
-      )
-    }
-    else{ //peer's message
-      return (
-        <><Box>
-          <Typography variant="body1" fontSize="15" fontWeight="500" sx={{ color: `${themePalette.yellow.main}` }}>
-            {onemessage.username}
+        </Box>
+        <Box sx={{ mb: 2 }}>
+          <Typography
+            variant="body1"
+            fontSize="15"
+            sx={{ color: `black.main` }}
+          >
+            {message.message}
           </Typography>
-        </Box><Box sx={{mb:2}}>
-            <Typography variant="body1" fontSize="15" sx={{ color: `${themePalette.black.main}` }}>
-              {onemessage.message}
-            </Typography>
-          </Box></>
-      )
-    }
-  })
+        </Box>
+      </Box>
+    );
+  });
 
-  //const messagesEndRef = useRef(null);
-  const messagesEndRef = useRef<null | HTMLDivElement>(null)
+  const { socket } = useSocket();
 
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+  const preventScroll = React.useRef(false);
   const scrollToBottom = () => {
-    messagesEndRef?.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    if (!preventScroll.current) {
+      messagesEndRef?.current?.scrollIntoView({ behavior: "smooth" });
+    }
 
-  useEffect(scrollToBottom, [{Messages}]);
+    preventScroll.current = false;
+  };
+  const sendMessage = (e: any) => {
+    e.preventDefault();
 
-  socket?.on("chat:receive_message",(payload)=>{
-    messages.push(payload.message);
-    setMessages(messages);
-  }); 
+    // prevent sending of empty texts
+    if (typedMessage.trim().length === 0) {
+      setTypedMessage("");
+      return;
+    }
+
+    if (socket && user) {
+      const payload = {
+        username: user.displayName,
+        message: typedMessage,
+      };
+
+      socket.emit("chat:message", payload);
+      setTypedMessage("");
+      setMessages((messages) => [payload, ...messages]);
+    } else {
+      console.error("Not connected to any socket");
+    }
+  };
+
+  const hasSocketListener = useRef(false);
+  useEffect(() => {
+    if (socket && !hasSocketListener.current) {
+      socket.on("chat:message", (payload) => {
+        // prevent scrolling to bottom on receive of message
+        preventScroll.current = true;
+
+        setMessages((messages) => [payload, ...messages]);
+      });
+
+      hasSocketListener.current = true;
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    // scroll to bottom when user sent a message
+    scrollToBottom();
+  }, [messages]);
 
   return (
     <Box
@@ -110,51 +126,66 @@ export default function ChatBox({ sx, ...rest }: ChatBoxProps) {
       }}
     >
       <Stack spacing={2}>
-        <Typography variant="h6" sx={{marginBottom: 3 }}>
-              Chat
+        <Typography variant="h6" sx={{ marginBottom: 3 }}>
+          Chat
         </Typography>
         <Box>
-          <Typography variant="body1" fontSize="15" fontWeight="100" align="center">
-              Messages sent will not be saved.
+          <Typography
+            variant="body1"
+            fontSize="15"
+            fontWeight="100"
+            align="center"
+          >
+            Messages sent will not be saved.
           </Typography>
         </Box>
-        <Box display="flex"
-            flexDirection="column"
-            height="400px"
-            sx={{overflow:"hidden", overflowY: "scroll"}}>
-          {Messages}
+        <Box
+          display="flex"
+          flexDirection="column-reverse"
+          height="400px"
+          sx={{ overflow: "hidden", overflowY: "scroll" }}
+        >
+          {/* Order of bottom is reversed as we are using column-reverse */}
           <div ref={messagesEndRef} />
+          {Messages}
         </Box>
-        <Box>
-          <TextField id="chat-textfield"
-                    variant="outlined"
-                    fullWidth
-                    type="text"
-                    value={typedMessage}
-                    onChange={(e) => setTypedMessage(e.target.value)}
-                    onKeyPress={(e)=>{if (e.key === 'Enter'&& typedMessage!=="") {postMessage()}}}
-                    placeholder="Start typing here..."
-                    sx={{
-                      "& .MuiOutlinedInput-root": {
-                        borderRadius: 0,
-                      },
-                      "& .MuiOutlinedInput-notchedOutline":
-                      {
-                        borderLeft:0,
-                        borderRight:0
-                      },
-                      "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
-                        {
-                          borderColor: "#67A0AC",
-                          borderRadius: 0,
-                          borderLeft: 0,
-                          borderRight: 0
-                        },
-                    }}
-                    //disabled={} - for when disconnected
+        <Box component="form" onSubmit={sendMessage}>
+          <TextField
+            id="chat-textfield"
+            variant="outlined"
+            fullWidth
+            type="text"
+            value={typedMessage}
+            onChange={(e) => setTypedMessage(e.target.value)}
+            placeholder="Start typing here..."
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton type="submit" edge="end">
+                    <SendIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: 0,
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderLeft: 0,
+                borderRight: 0,
+              },
+              "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                {
+                  borderColor: "#67A0AC",
+                  borderRadius: 0,
+                  borderLeft: 0,
+                  borderRight: 0,
+                },
+            }}
           />
-        </Box> 
-    </Stack>
+        </Box>
+      </Stack>
     </Box>
   );
 }
