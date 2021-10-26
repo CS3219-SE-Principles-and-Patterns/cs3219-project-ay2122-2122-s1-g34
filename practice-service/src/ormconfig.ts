b658a73c312d661543bcf0481f7d44d89fbaff75
nelsontky <nelson@ccb.wtf>
2021-10-26 15:38:58 +0000

    feat: update .env

--- practice-service/src/ormconfig.ts
+++ practice-service/src/ormconfig.ts
@@ -1,24 +1,16 @@
-import * as dotenv from "dotenv";
-import * as fs from "fs";
 import { ConnectionOptions } from "typeorm";
 
 import { Question } from "./questions/entities/question.entity";
 import { SessionNote } from "./session-notes/entities/session-note.entity";
 import { Session } from "./sessions/entities/session.entity";
 
-const data: any = dotenv.parse(
-  fs.readFileSync(
-    process.env.NODE_ENV === "production" ? ".env.production" : ".env"
-  )
-);
-
 const config: ConnectionOptions = {
   type: "postgres",
-  host: data.POSTGRES_HOST,
-  port: +data.POSTGRES_PORT,
-  username: data.POSTGRES_USER,
-  password: data.POSTGRES_PASSWORD,
-  database: data.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_USER,
   entities: [Question, Session, SessionNote],
   synchronize: false,
   migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
