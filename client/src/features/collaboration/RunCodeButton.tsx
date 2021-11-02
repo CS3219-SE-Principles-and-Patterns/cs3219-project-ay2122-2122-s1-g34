60ddf21addc553893fc66a9a410f3068ab2c9d95
nelsontky <nelson@ccb.wtf>
2021-11-02 13:09:12 +0000

    refactor: remove console.log

--- client/src/features/collaboration/RunCodeButton.tsx
+++ client/src/features/collaboration/RunCodeButton.tsx
@@ -87,7 +87,6 @@ export default function RunCodeButton({
                 { headers: { token: user.token } }
               );
 
-              console.log(response.data);
               setRunResult(JSON.stringify(response.data));
             } finally {
               setLoading(false);
