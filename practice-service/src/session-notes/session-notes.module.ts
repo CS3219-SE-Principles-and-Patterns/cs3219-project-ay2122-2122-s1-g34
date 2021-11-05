fa3f9006b1e88bfc503ff15536f26df3029b48de
nelsontky <nelson@ccb.wtf>
2021-11-05 13:04:29 +0000

    fix: authorize users before they edit a session note

--- practice-service/src/session-notes/session-notes.module.ts
+++ practice-service/src/session-notes/session-notes.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { Session } from "../sessions/entities/session.entity";
 import { SessionNote } from "./entities/session-note.entity";
 import { SessionNotesController } from "./session-notes.controller";
 import { SessionNotesService } from "./session-notes.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SessionNote])],
+  imports: [TypeOrmModule.forFeature([Session, SessionNote])],
   controllers: [SessionNotesController],
   providers: [SessionNotesService],
 })
--- practice-service/src/session-notes/session-notes.service.ts
+++ practice-service/src/session-notes/session-notes.service.ts
@@ -1,20 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
 import { InjectRepository } from "@nestjs/typeorm";
 import { plainToClass } from "class-transformer";
 import { Repository } from "typeorm";
 
+import { Session, Status } from "../sessions/entities/session.entity";
 import { UpdateSessionNoteDto } from "./dto/update-session-note.dto";
 import { SessionNote } from "./entities/session-note.entity";
 
 @Injectable()
 export class SessionNotesService {
   constructor(
+    @InjectRepository(Session)
+    private readonly sessionsRepository: Repository<Session>,
     @InjectRepository(SessionNote)
     private readonly sessionNotesRepository: Repository<SessionNote>
   ) {}
 
   async updateSessionNote(updateSessionNoteDto: UpdateSessionNoteDto) {
     const { userId, sessionId, note } = updateSessionNoteDto;
+
+    const session = await this.sessionsRepository
+      .createQueryBuilder("session")
+      .where("session.allowedUserIds @> (:userId)", {
+        userId: [userId],
+      })
+      .andWhere("session.status = :status", { status: Status.Closed })
+      .andWhere("session.id = :id", { id: sessionId })
+      .getOne();
+
+    if (!session) {
+      throw new RpcException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: "You are not allowed to edit this note!",
+        error: "Forbidden",
+      });
+    }
 
     const updatedNote = plainToClass(SessionNote, {
       userId,
